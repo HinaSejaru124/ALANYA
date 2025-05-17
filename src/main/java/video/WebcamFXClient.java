13637a11515d95ad6c8017f2b193bda19aac8810
HinaSejaru124 <bissecknathanael@gmail.com>
2025-05-17 09:45:44 +0100

    Video

--- src/main/java/video/WebcamFXClient.java
+++ src/main/java/video/WebcamFXClient.java
@@ -1,13 +1,18 @@
 package video;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
-
-import com.github.sarxos.webcam.Webcam;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -20,38 +25,110 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class WebcamFXClient extends Application {
-    private Webcam webcam;
+    private static final String HOST = "localhost";
+    private static final int VIDEO_PORT = 2000;
+    private static final int AUDIO_PORT = 2001;
+
     private ImageView imageView;
+    private SourceDataLine speakers;
+
+    private PriorityBlockingQueue<VideoFrame> videoQueue = new PriorityBlockingQueue<>();
+    private PriorityBlockingQueue<AudioChunk> audioQueue = new PriorityBlockingQueue<>();
 
     @Override
     public void start(Stage stage) {
-        new Thread(() -> {
-            try (Socket socket = new Socket("localhost", 2000)) {
-                DataInputStream dis = new DataInputStream(socket.getInputStream());
-                while (true) {
-                    try {
-                        Image fxImage = convertToFxImage(ImageIO.read(dis));
-                        Platform.runLater(() -> imageView.setImage(fxImage));
-                    } catch (IOException e) {}
-                }
-            } catch (IOException e) {
-            }
-        }).start();
-
         imageView = new ImageView();
         StackPane root = new StackPane(imageView);
-        Scene scene = new Scene(root, 640, 480);
-
-        stage.setScene(scene);
-        stage.setTitle("Webcam avec JavaFX");
-        stage.setOnCloseRequest(e -> webcam.close()); // Fermer la webcam lors de la fermeture
+        stage.setScene(new Scene(root, 640, 480));
+        stage.setTitle("Webcam synchronisée");
         stage.show();
+
+        initAudioLine();
+        startVideoReceiver();
+        startAudioReceiver();
+
+        new Thread(this::playoutLoop).start();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void initAudioLine() {
+        try {
+            AudioFormat format = new AudioFormat(44100, 16, 2, true, false); // little-endian
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            speakers = (SourceDataLine) AudioSystem.getLine(info);
+            speakers.open(format);
+            speakers.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startVideoReceiver() {
+        new Thread(() -> {
+            try (Socket socket = new Socket(HOST, VIDEO_PORT);
+                 DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+                while (true) {
+                    long ts = dis.readLong();
+                    int length = dis.readInt();
+                    byte[] data = new byte[length];
+                    dis.readFully(data);
+                    BufferedImage bufImg = ImageIO.read(new ByteArrayInputStream(data));
+                    if (bufImg != null) {
+                        videoQueue.put(new VideoFrame(ts, bufImg));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void startAudioReceiver() {
+        new Thread(() -> {
+            try (Socket socket = new Socket(HOST, AUDIO_PORT);
+                 DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+
+                while (true) {
+                    long ts = dis.readLong();
+                    int length = dis.readInt();
+                    byte[] buffer = new byte[length];
+                    dis.readFully(buffer);
+                    audioQueue.put(new AudioChunk(ts, buffer, length));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void playoutLoop() {
+        try {
+            while (true) {
+                long now = System.nanoTime();
+                VideoFrame vf = videoQueue.peek();
+                if (vf != null && vf.timestamp <= now) {
+                    videoQueue.poll();
+                    Image fxImage = convertToFxImage(vf.bufferedImage);
+                    Platform.runLater(() -> imageView.setImage(fxImage));
+                }
+                AudioChunk ac = audioQueue.peek();
+                if (ac != null && ac.timestamp <= now) {
+                    audioQueue.poll();
+                    speakers.write(ac.data, 0, ac.length);
+                }
+                TimeUnit.MILLISECONDS.sleep(5);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
     private Image convertToFxImage(BufferedImage bufferedImage) {
         WritableImage writableImage = new WritableImage(bufferedImage.getWidth(), bufferedImage.getHeight());
         PixelWriter pw = writableImage.getPixelWriter();
-
         for (int y = 0; y < bufferedImage.getHeight(); y++) {
             for (int x = 0; x < bufferedImage.getWidth(); x++) {
                 pw.setArgb(x, y, bufferedImage.getRGB(x, y));
@@ -60,7 +137,29 @@ public class WebcamFXClient extends Application {
         return writableImage;
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    private static class VideoFrame implements Comparable<VideoFrame> {
+        final long timestamp;
+        final BufferedImage bufferedImage;
+        VideoFrame(long ts, BufferedImage img) {
+            this.timestamp = ts;
+            this.bufferedImage = img;
+        }
+        @Override public int compareTo(VideoFrame o) {
+            return Long.compare(this.timestamp, o.timestamp);
+        }
+    }
+
+    private static class AudioChunk implements Comparable<AudioChunk> {
+        final long timestamp;
+        final byte[] data;
+        final int length;
+        AudioChunk(long ts, byte[] data, int len) {
+            this.timestamp = ts;
+            this.data = data;
+            this.length = len;
+        }
+        @Override public int compareTo(AudioChunk o) {
+            return Long.compare(this.timestamp, o.timestamp);
+        }
     }
 }
--- src/main/java/video/WebcamFXServer.java
+++ src/main/java/video/WebcamFXServer.java
@@ -1,43 +1,84 @@
 package video;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import javax.imageio.ImageIO;
-
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamResolution;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
 
 public class WebcamFXServer {
-    private Webcam webcam;
+    private static final int VIDEO_PORT = 2000;
+    private static final int AUDIO_PORT = 2001;
+
+    public static void main(String[] args) throws Exception {
+        new WebcamFXServer().start();
+    }
 
     public void start() {
-        webcam = Webcam.getDefault();
-        webcam.setViewSize(WebcamResolution.VGA.getSize());
-        webcam.open();
-
+        // Thread vidéo
         new Thread(() -> {
-            try (ServerSocket serverSocket = new ServerSocket(2000)) {
-                Socket socket = serverSocket.accept();
-                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            try (ServerSocket serverSocket = new ServerSocket(VIDEO_PORT);
+                 Socket socket = serverSocket.accept();
+                 DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
+
+                // Libération de la webcam
+                com.github.sarxos.webcam.Webcam webcam = com.github.sarxos.webcam.Webcam.getDefault();
+                webcam.setViewSize(com.github.sarxos.webcam.WebcamResolution.VGA.getSize());
+                webcam.open();
+
                 while (true) {
-                    BufferedImage bufferedImage = webcam.getImage();
-                    if (bufferedImage != null) {
-                        ImageIO.write(bufferedImage, "JPG", dos);
+                    long ts = System.nanoTime();
+                    BufferedImage img = webcam.getImage();
+                    if (img != null) {
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        ImageIO.write(img, "JPG", baos);
+                        byte[] data = baos.toByteArray();
+
+                        dos.writeLong(ts);               // Timestamp
+                        dos.writeInt(data.length);      // Longueur
+                        dos.write(data);                // Image
                         dos.flush();
                     }
                 }
-            } catch (IOException ex) {
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
         }).start();
-    }
 
-    public static void main(String[] args) {
-        new WebcamFXServer().start();
-        System.out.println("Server on");
+        // Thread audio
+        new Thread(() -> {
+            AudioFormat format = new AudioFormat(44100, 16, 2, true, true);
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            try (ServerSocket serverSocket = new ServerSocket(AUDIO_PORT);
+                 Socket socket = serverSocket.accept();
+                 DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
+
+                TargetDataLine mic = (TargetDataLine) AudioSystem.getLine(info);
+                mic.open(format);
+                mic.start();
+
+                byte[] buffer = new byte[4096];
+                while (true) {
+                    int count = mic.read(buffer, 0, buffer.length);
+                    if (count > 0) {
+                        long ts = System.nanoTime();
+                        dos.writeLong(ts);           // Timestamp
+                        dos.writeInt(count);         // Nombre d'octets
+                        dos.write(buffer, 0, count);
+                        dos.flush();
+                    }
+                }
+            } catch (LineUnavailableException | IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
